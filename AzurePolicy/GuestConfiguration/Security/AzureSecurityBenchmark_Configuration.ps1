897af2e4022b250befa64c68756670d4f78d0cca
William Sanders <6565467+wzsanders@users.noreply.github.com>
2022-05-08 18:22:24 -0500

    Update Azure Security Guest Configuration

--- AzurePolicy/GuestConfiguration/Security/AzureSecurityBenchmark_Configuration.ps1
+++ AzurePolicy/GuestConfiguration/Security/AzureSecurityBenchmark_Configuration.ps1
@@ -1,6 +1,7 @@
 Configuration AzureSecurityBenchmark_Configuration
 {
     Import-DscResource -ModuleName 'PSDscResources'
+    Import-DscResource -ModuleName 'SecurityPolicyDsc'
 
     Node localhost
     {
@@ -52,7 +53,51 @@ Configuration AzureSecurityBenchmark_Configuration
         UserRightsAssignment "AZ-WIN-00184: Bypass traverse checking" {
             Policy   = 'Bypass_traverse_checking'
             Identity = 'Administrators', 'Authenticated Users', 'Backup Operators', 'LOCAL SERVICE', 'NETWORK SERVICE'
+            Force    = $true
         }
+
+        UserRightsAssignment "AZ-WIN-00185: Increase a process working set" {
+            Policy   = 'Increase_a_process_working_set'
+            Identity = 'Administrators', 'LOCAL SERVICE'
+            Force    = $true
+        }
+
+        UserRightsAssignment "CCE-35818-4: Configure 'Access this computer from the network'" {
+            Policy   = 'Access_this_computer_from_the_network'
+            Identity = 'Administrators', 'Authenticated Users'
+            Force    = $true
+        }
+
+        SecurityOption "CCE-36077-6: Ensure 'Network access: Do not allow anonymous enumeration of SAM accounts' is set to 'Enabled' (MS only)" {
+            Name                                                              = 'Network_access_Do_not_allow_anonymous_enumeration_of_SAM_accounts'
+            Network_access_Do_not_allow_anonymous_enumeration_of_SAM_accounts = 'Enabled'
+        }
+
+        UserRightsAssignment "CCE-36867-0: Ensure 'Deny log on through Remote Desktop Services' is configured" {
+            Policy   = 'Deny_log_on_through_Remote_Desktop_Services'
+            Identity = ''
+            Force    = $true
+        }
+
+        Registry "CCE-37346-4: Ensure 'Enable RPC Endpoint Mapper Client Authentication' is set to 'Disabled'" {
+            Key       = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Rpc'
+            Ensure    = 'Present'
+            ValueName = 'EnableAuthEpResolution'
+            ValueData = 1
+            ValueType = 'DWord'
+            Force     = $true
+        }
+
+        UserRightsAssignment "CCE-37659-0: Configure 'Allow log on locally'" {
+            Policy   = 'Allow_log_on_locally'
+            Identity = 'Administrators'
+            Force    = $true
+        }
+
+        # SecurityOption "CCE-37741-6: Ensure 'Audit Credential Validation' is set to 'Success and Failure'" {
+        #     Name = 'Audit_Credential_Validation'
+        #     Audit_Credential_Validation = 'Success and Failure'
+        # }
     }
 }
 
