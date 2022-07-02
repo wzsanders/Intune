1c170be29dc71cbfb78796ba66cbf25b91b7f5c1
William Sanders <6565467+wzsanders@users.noreply.github.com>
2022-07-02 01:56:25 -0500

    Update Tailscale Package

--- MicrosoftEndpointManager/IntuneWinApp/Tailscale/src/uninstall.ps1
+++ MicrosoftEndpointManager/IntuneWinApp/Tailscale/src/uninstall.ps1
@@ -2,7 +2,7 @@ Stop-Service -Name Tailscale -ErrorAction SilentlyContinue
 
 choco uninstall tailscale -y
 
-$TailscalePath = "$env:ProgramFiles\Tailscale IPN"
+$TailscalePath = (Get-ChildItem -Path 'C:\Program Files\','C:\Program Files (x86)\' -Filter 'Tailscale IPN' -ErrorAction SilentlyContinue).FullName
 
 if (Test-Path -Path $TailscalePath) {
     Remove-Item -Path $TailscalePath -Recurse -Force -ErrorAction SilentlyContinue
